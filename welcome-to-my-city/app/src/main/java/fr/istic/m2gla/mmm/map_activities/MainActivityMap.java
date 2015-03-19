d917f339decb2a75590a6456288102b92c423207
amhachi <m.amhachi@gmail.com>
2015-03-19 21:35:20 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	welcome-to-my-city/app/src/main/java/fr/istic/m2gla/mmm/map_activities/MainActivityMap.java

--- welcome-to-my-city/app/src/main/java/fr/istic/m2gla/mmm/map_activities/MainActivityMap.java
+++ welcome-to-my-city/app/src/main/java/fr/istic/m2gla/mmm/map_activities/MainActivityMap.java
@@ -1,6 +1,8 @@
 package fr.istic.m2gla.mmm.map_activities;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.SQLException;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -26,6 +28,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+
+import fr.istic.m2gla.mmm.DataProvider;
 import fr.istic.m2gla.mmm.R;
 import fr.istic.m2gla.mmm.client.Common;
 import fr.istic.m2gla.mmm.client.Constants;
@@ -33,12 +37,11 @@ import fr.istic.m2gla.mmm.client.Constants;
 /**
  * MainActivity.
  * ²
- *
  */
-public class MainActivityMap extends Activity implements LocationListener{
+public class MainActivityMap extends Activity implements LocationListener {
 
     private LocationManager locationManager;
-    private Location location ;
+    private Location location;
     private GoogleMap gMap;
     private Marker marker;
 
@@ -50,7 +53,10 @@ public class MainActivityMap extends Activity implements LocationListener{
     @Override
     protected void onStart() {
         super.onStart();
+
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
         new HttpRequestTask().execute();
+        else Toast.makeText(this,"SVP, activer le GPS", Toast.LENGTH_LONG).show();
     }
 
 
@@ -58,23 +64,16 @@ public class MainActivityMap extends Activity implements LocationListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        gMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-        gMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
 
         //Obtention de la référence du service
         locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
         location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        //marker = gMap.addMarker(new MarkerOptions().title("Vous êtes ici").position(new LatLng(0, 0)));
-
-
- /*     // latitude and longitude
-      double latitude = 48.1119800 ;
-      double longitude = -1.6742900;*/
-
-
-
     }
+
+
 
     // methode afficher positions
     public void createMarketOfPerson(double latitude, double longitude, Bitmap image, String email) {
@@ -91,11 +90,39 @@ public class MainActivityMap extends Activity implements LocationListener{
 
         canvas.drawBitmap(Bitmap.createScaledBitmap(image, 50, 30, true), 0, 0, color);
 
-        gMap.addMarker(new MarkerOptions()
+        MarkerOptions markerOptions = new MarkerOptions()
                 .position(new LatLng(latitude, longitude))
                 .icon(BitmapDescriptorFactory.fromBitmap(bmp))
                 .anchor(0.5f, 1)
-                .title(email)).showInfoWindow();
+                .title(email);
+
+
+        gMap.addMarker(markerOptions).showInfoWindow();
+        gMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String email = marker.getTitle();
+                if (!isEmailValid(email)) {
+                    Toast.makeText(MainActivityMap.this, "Email invalid.", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+
+                try {
+                    ContentValues values = new ContentValues(2);
+                    values.put(DataProvider.COL_NAME, email.substring(0, email.indexOf('@')));
+                    values.put(DataProvider.COL_EMAIL, email);
+                    MainActivityMap.this.getContentResolver().insert(DataProvider.CONTENT_URI_PROFILE, values);
+                } catch (SQLException sqle) {
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+    private boolean isEmailValid(String email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
     /**
@@ -109,7 +136,7 @@ public class MainActivityMap extends Activity implements LocationListener{
         locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
 
         //Si le GPS est disponible, on s'y abonne
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             abonnementGPS();
         }
     }
@@ -130,7 +157,8 @@ public class MainActivityMap extends Activity implements LocationListener{
      */
     public void abonnementGPS() {
         //On s'abonne
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500000000, 100000000, this);
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 999999999, 999999999, this);
     }
 
     /**
@@ -150,6 +178,7 @@ public class MainActivityMap extends Activity implements LocationListener{
         final StringBuilder msg = new StringBuilder("lat : ");
         msg.append(location.getLatitude());
         msg.append( "; lng : ");
+
         msg.append(location.getLongitude());
 
         Toast.makeText(this, msg.toString(), Toast.LENGTH_SHORT).show();
@@ -168,7 +197,9 @@ public class MainActivityMap extends Activity implements LocationListener{
 
     public void onProviderDisabled(final String provider) {
         //Si le GPS est désactivé on se désabonne
+
         if("gps".equals(provider)) {
+
             desabonnementGPS();
         }
     }
@@ -180,6 +211,7 @@ public class MainActivityMap extends Activity implements LocationListener{
     public void onProviderEnabled(final String provider) {
         //Si le GPS est activé on s'abonne
         if("gps".equals(provider)) {
+
             abonnementGPS();
         }
     }
@@ -188,7 +220,9 @@ public class MainActivityMap extends Activity implements LocationListener{
      * {@inheritDoc}
      */
 
-    public void onStatusChanged(final String provider, final int status, final Bundle extras) { }
+
+    public void onStatusChanged(final String provider, final int status, final Bundle extras) {
+    }
 
 
     private class HttpRequestTask extends AsyncTask<Void, Void, Contact[]> {
@@ -197,7 +231,7 @@ public class MainActivityMap extends Activity implements LocationListener{
         protected Contact[] doInBackground(Void... params) {
             try {
                 final LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                final String url = Constants.SERVER_URL+"rest/contact/email="+ Common.getPreferredEmail()+"&lat="+latLng.latitude+"&long="+latLng.longitude;
+                final String url = Constants.SERVER_URL + "rest/contact/email=" + Common.getPreferredEmail() + "&lat=" + latLng.latitude + "&long=" + latLng.longitude;
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
                 ResponseEntity<Contact[]> listCoordibantes = restTemplate.getForEntity(url, Contact[].class);
@@ -212,7 +246,7 @@ public class MainActivityMap extends Activity implements LocationListener{
 
         @Override
         protected void onPostExecute(Contact[] coordonnees) {
-            for (Contact userCoordinates : coordonnees){
+            for (Contact userCoordinates : coordonnees) {
                 Bitmap bitmapImg = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.person), 100, 75, true);
                 createMarketOfPerson(userCoordinates.getLatitude(), userCoordinates.getLongitude(), bitmapImg, userCoordinates.getEmail());
             }
